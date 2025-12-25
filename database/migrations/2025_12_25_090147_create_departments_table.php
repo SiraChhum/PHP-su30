034799858e6408ef77f2e1f77f463c35de44764f
chhumsira-kh <sirachhum.kh@gmail.com>
2025-12-25 16:25:09 +0700

    migrate

---
+++ database/migrations/2025_12_25_090147_create_departments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('department', function (Blueprint $table) {
+            $table->id('department_id');
+            $table->string('department_code')->unique();
+            $table->string('department_name');
+            $table->text('department_description')->nullable();
+            $table->enum('department_status', ['active', 'inactive'])->default('active');
+            $table->timestamps(); // created_at and updated_at
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('department');
+    }
+};
